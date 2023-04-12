e0e27bd31b1123ff4b16a781df9891a07c5c054b
OmarHany23 <omarhany2k@gmail.com>
2023-04-12 16:08:16 +0200

    update static images & fixing site css

--- static/js/upload.js
+++ static/js/upload.js
@@ -1,24 +1,52 @@
-var btnUpload = $("#upload_file"),
-		btnOuter = $(".button_outer");
-	btnUpload.on("change", function(e){
-		var ext = btnUpload.val().split('.').pop().toLowerCase();
-		if($.inArray(ext, ['gif','png','jpg','jpeg']) == -1) {
-			$(".error_msg").text("Not an Image...");
-		} else {
-			$(".error_msg").text("");
-			btnOuter.addClass("file_uploading");
-			setTimeout(function(){
-				btnOuter.addClass("file_uploaded");
-			},3000);
-			var uploadedFile = URL.createObjectURL(e.target.files[0]);
-			setTimeout(function(){
-				$("#uploaded_view").append('<img src="'+uploadedFile+'" />').addClass("show");
-			},3500);
-		}
-	});
-	$(".file_remove").on("click", function(e){
-		$("#uploaded_view").removeClass("show");
-		$("#uploaded_view").find("img").remove();
-		btnOuter.removeClass("file_uploading");
-		btnOuter.removeClass("file_uploaded");
-	});
+//from https://www.codingnepalweb.com/drag-drop-file-upload-feature-javascript/
+//selecting all required elements
+const dropArea = document.querySelector(".drag-area"),
+dragText = dropArea.querySelector("header"),
+button = dropArea.querySelector("button"),
+input = dropArea.querySelector("input");
+let file; //this is a global variable and we'll use it inside multiple functions
+button.onclick = ()=>{
+  input.click(); //if user click on the button then the input also clicked
+}
+input.addEventListener("change", function(){
+  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+  file = this.files[0];
+  dropArea.classList.add("active");
+  showFile(); //calling function
+});
+//If user Drag File Over DropArea
+dropArea.addEventListener("dragover", (event)=>{
+  event.preventDefault(); //preventing from default behaviour
+  dropArea.classList.add("active");
+  dragText.textContent = "Release to Upload File";
+});
+//If user leave dragged File from DropArea
+dropArea.addEventListener("dragleave", ()=>{
+  dropArea.classList.remove("active");
+  dragText.textContent = "Drag & Drop to Upload File";
+});
+//If user drop File on DropArea
+dropArea.addEventListener("drop", (event)=>{
+  event.preventDefault(); //preventing from default behaviour
+  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+  file = event.dataTransfer.files[0];
+  showFile(); //calling function
+});
+function showFile(){
+  let fileType = file.type; //getting selected file type
+  let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //adding some valid image extensions in array
+  if(validExtensions.includes(fileType)){ //if user selected file is an image file
+    let fileReader = new FileReader(); //creating new FileReader object
+    fileReader.onload = ()=>{
+      let fileURL = fileReader.result; //passing user file source in fileURL variable
+        // UNCOMMENT THIS BELOW LINE. I GOT AN ERROR WHILE UPLOADING THIS POST SO I COMMENTED IT
+       let imgTag = `<img src="${fileURL}" alt="image">`; //creating an img tag and passing user selected file source inside src attribute
+      dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
+    }
+    fileReader.readAsDataURL(file);
+  }else{
+    alert("This is not an Image File!");
+    dropArea.classList.remove("active");
+    dragText.textContent = "Drag & Drop to Upload File";
+  }
+}
